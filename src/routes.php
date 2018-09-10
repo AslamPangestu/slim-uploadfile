5fdcf2f5807fe97ebbd63a87b3d30b9b43eabc03
Muhammad Aslam Pangestu Idham <aslampangestu@gmail.com>
2018-09-10 15:47:40 +0700

    recreate route

--- src/routes.php
+++ src/routes.php
@@ -22,37 +22,30 @@ $app->get("/qrcode/", function (Request $request, Response $response){
     return $response->withJson(["status" => "success", "data" => $result], 200);
 });
 
+//search data
+$app->get("/qrcode/search/", function (Request $request, Response $response, $args){
+    $keyword = $request->getQueryParam("content");
+    $sql = "SELECT * FROM tbl_generated_qrcode WHERE content LIKE '%$keyword%'";
+    $stmt = $this->db->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $response->withJson(["status" => "success", "data" => $result], 200);
+});
+
+
 //add new data
 $app->post("/qrcode/", function (Request $request, Response $response){
     $new_data = $request->getParsedBody();
-    //get image
-    $uploadedFiles = $request->getUploadedFiles();
-    $uploadedFile = $uploadedFiles['qrcode_path'];
 
-    //cek upload success & will safe in tmp
-    if($uploadedFile->getError() === UPLOAD_ERR_OK){
-        $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+    $sql = "INSERT INTO tbl_generated_qrcode (content) VALUE (:content)";
+    $stmt = $this->db->prepare($sql);
 
-        // ubah nama file dengan id buku
-        $filename = sprintf('%s.%0.8s', $args["id"], $extension);
-        
-        $directory = $this->get('settings')['upload_directory'];
-        $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+    $data = [
+        ":content" => $new_data["content"]
+    ];
 
-        $sql = "INSERT INTO tbl_generated_qrcode (content, qrcode_path) VALUE (:content, :qrcode_path)";
-        $stmt = $this->db->prepare($sql);
-
-        $data = [
-            ":content" => $new_data["content"],
-            ":qrcode_path" => $filename
-        ];
-
-        if($stmt->execute($data)){
-            // ambil base url dan gabungkan dengan file name untuk membentuk URL file
-            $url = $request->getUri()->getBaseUrl()."/uploads/".$filename;
-            return $response->withJson(["status" => "success", "data" => "1"], 200);
-        }
-        
-        return $response->withJson(["status" => "failed", "data" => "0"], 200);
-    }
+    if($stmt->execute($data))
+       return $response->withJson(["status" => "success", "data" => "1"], 200);
+    
+    return $response->withJson(["status" => "failed", "data" => "0"], 200);
 });
